bce60b5415c78604f56790c749313a363554c54a
GodyRacks <godfreymatagaro@gmail.com>
2023-08-30 11:10:28 -0700

    betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
-/*
- * _print_rev_recursion - prints a string in reverse
- * @s - the string to be printed
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to be printed
  *
  * Return: void
  */
